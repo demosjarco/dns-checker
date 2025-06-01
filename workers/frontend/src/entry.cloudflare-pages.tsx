c01c1053da5122617f6c49c38b40da7e9dc72020
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-06-01 13:21:22 -0700

    Create entry.cloudflare-pages.tsx

---
+++ workers/frontend/src/entry.cloudflare-pages.tsx
@@ -0,0 +1,30 @@
+/*
+ * WHAT IS THIS FILE?
+ *
+ * It's the entry point for Cloudflare Pages when building for production.
+ *
+ * Learn more about the Cloudflare Pages integration here:
+ * - https://qwik.dev/docs/deployments/cloudflare-pages/
+ *
+ */
+import { createQwikCity, type PlatformCloudflarePages } from '@builder.io/qwik-city/middleware/cloudflare-pages';
+import qwikCityPlan from '@qwik-city-plan';
+import type { PlatformProxy } from 'wrangler';
+import render from './entry.ssr';
+
+declare global {
+	interface QwikCityPlatformLive extends Omit<PlatformCloudflarePages, 'request' | 'env' | 'ctx'> {
+		request: Request;
+		env: Env;
+		ctx: ExecutionContext;
+		cf?: never;
+	}
+	interface QwikCityPlatformLocal extends Omit<PlatformProxy<Env>, 'request'> {
+		request?: never;
+	}
+	type QwikCityPlatform = QwikCityPlatformLive | QwikCityPlatformLocal;
+}
+
+const fetch = createQwikCity({ render, qwikCityPlan });
+
+export { fetch };
