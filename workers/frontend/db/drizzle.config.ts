5593856d9905272f0e3463cfb70f78920066bdee
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-06-01 19:42:29 -0700

    Fix paths

--- workers/frontend/db/drizzle.config.ts
+++ workers/frontend/db/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
+
 export default defineConfig({
 	dialect: 'sqlite',
 	driver: 'd1-http',
-	schema: './schema.ts',
+	schema: './db/schema.ts',
 	casing: 'snake_case',
-	out: './migrations',
+	out: './db/migrations',
 	dbCredentials: {
 		accountId: process.env['CF_ACCOUNT_ID']!,
 		databaseId: '6d73e12f-0ccc-4c74-be05-e70e981eb1cd',
