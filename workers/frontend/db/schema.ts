c03099bb64ae35c7cd2bb898aa9a6667106ce47a
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-06-02 14:45:17 -0700

    Just worry about `iata`

--- workers/frontend/db/schema.ts
+++ workers/frontend/db/schema.ts
@@ -1,6 +1,6 @@
 import type { DOLocations } from '@chainfuse/types';
 import { sql, type SQL } from 'drizzle-orm';
-import { sqliteTable, unique, uniqueIndex, type AnySQLiteColumn } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, uniqueIndex, type AnySQLiteColumn } from 'drizzle-orm/sqlite-core';
 
 /**
  * @returns a copy of string `x` with all ASCII characters converted to lower case
@@ -19,23 +19,18 @@ export const locations = sqliteTable(
 	(l) => [uniqueIndex('case_insensitive_location').on(lower(l.location))],
 );
 
-export const instances = sqliteTable(
-	'instances',
-	(i) => ({
-		doId: i.blob({ mode: 'buffer' }).primaryKey().notNull(),
-		/**
-		 * @deprecated DO NOT USE (BufferHelpers is faster and cheaper)
-		 */
-		doId_hex: i.text().generatedAlwaysAs((): SQL => sql`lower(hex(${instances.doId}))`, { mode: 'virtual' }),
-		location: i
-			.text({ mode: 'text' })
-			.notNull()
-			.$type<DOLocations>()
-			.references(() => locations.location, { onUpdate: 'cascade', onDelete: 'cascade' }),
-		iata: i.text({ mode: 'text' }).notNull(),
-		colo: i.integer({ mode: 'number' }).notNull(),
-		iso_country: i.text({ mode: 'text' }).notNull(),
-		iso_region: i.text({ mode: 'text' }),
-	}),
-	(i) => [unique().on(i.iata, i.colo)],
-);
+export const instances = sqliteTable('instances', (i) => ({
+	doId: i.blob({ mode: 'buffer' }).primaryKey().notNull(),
+	/**
+	 * @deprecated DO NOT USE (BufferHelpers is faster and cheaper)
+	 */
+	doId_hex: i.text().generatedAlwaysAs((): SQL => sql`lower(hex(${instances.doId}))`, { mode: 'virtual' }),
+	location: i
+		.text({ mode: 'text' })
+		.notNull()
+		.$type<DOLocations>()
+		.references(() => locations.location, { onUpdate: 'cascade', onDelete: 'cascade' }),
+	iata: i.text({ mode: 'text' }).unique().notNull(),
+	iso_country: i.text({ mode: 'text' }).notNull(),
+	iso_region: i.text({ mode: 'text' }),
+}));
