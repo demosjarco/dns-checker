750b57ae7f29791f2659eedd8e6acb943a4b0a36
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-06-01 17:23:33 -0700

    Update entry.cloudflare-workers.ts

--- workers/frontend/src/entry.cloudflare-workers.ts
+++ workers/frontend/src/entry.cloudflare-workers.ts
@@ -4,5 +4,8 @@ import { fetch } from '../server/entry.cloudflare-pages';
 export { LocationTester } from '../do/locationTester.mjs';
 
 export default {
+	/**
+	 * @link https://qwik.dev/docs/deployments/cloudflare-pages/#cloudflare-pages-entry-middleware
+	 */
 	fetch,
 } satisfies ExportedHandler<EnvVars>;
